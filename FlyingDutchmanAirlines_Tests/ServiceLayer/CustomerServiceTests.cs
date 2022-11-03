aa67c07f82094ec5a085fef7099d2a1ae01e7db1
dimitriuz <dimitriuz@gmail.com>
2022-11-03 20:55:04 +0300

    Serive layer

---
+++ FlyingDutchmanAirlines_Tests/ServiceLayer/CustomerServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyingDutchmanAirlines_Tests.ServiceLayer
+{
+    [TestClass]
+    internal class CustomerServiceTests
+    {
+        [TestInitialize]
+        public async Task TestInitialize()
+        { 
+
+        }
+    }
+}
